ea1fd82a8df2e8b1cc65eca0eca8e0fda2780c46
Nathan Ho <nkh6243@gmail.com>
2025-05-21 19:00:08 -0500

    initial springboot skeleton

---
+++ CoffeeApp/src/test/java/edu/depaul/coffeeapp/CoffeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.coffeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
