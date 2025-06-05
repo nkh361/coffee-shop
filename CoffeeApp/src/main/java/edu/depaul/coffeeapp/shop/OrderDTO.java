977a605f90f901643efd8dee7cd24e4c8efb696e
Nathan Ho <nkh6243@gmail.com>
2025-06-04 23:08:19 -0500

    lombok'd orderDTO

--- CoffeeApp/src/main/java/edu/depaul/coffeeapp/shop/OrderDTO.java
+++ CoffeeApp/src/main/java/edu/depaul/coffeeapp/shop/OrderDTO.java
@@ -1,20 +1,19 @@
 package edu.depaul.coffeeapp.shop;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
     private Long orderId;
     private String customerName;
     private double shopId;
     private LocalDateTime orderTime;
     private List<String> items;
-
-    public OrderDTO(Long id, String customer, double shopId, LocalDateTime orderTime, List<String> items) {
-        this.orderId = id;
-        this.customerName = customer;
-        this.shopId = shopId;
-        this.orderTime = orderTime;
-        this.items = items;
-    }
 }
